feb140851442ab24147fef6cce541a90d10f9848
Christian Ocklind <christian.ocklind.0572@student.uu.se>
2021-12-13 21:56:18 +0100

    IR_BLACK theme

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Your Name"
-      user-mail-address "your.name@email.com")
+(setq user-full-name "Christian Ocklind"
+      user-mail-address "christian.ocklind@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-ir-black)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
